451537ccad9166201f7dcffe7a80aa094b3d8390
vpractice <68060284+vpractice@users.noreply.github.com>
2020-08-29 23:15:10 +0500

    Updating source code for comments
    Updating source code for comments

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -14,7 +14,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  /**
  * Wakaleo Consulting - John Ferguson Smart
  * Game of life, demonstration application for Jenkins: The Definitive Guide
- * re-packaged by vPractice Developers
+ * re-packaged by vPractice Developers itech.local
  * GameController.java
  * Controller to navigate the buttons on the game pages
  */
